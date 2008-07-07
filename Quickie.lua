4acb461ed4281958bb2d76db7232b132f344f70c
Tekkub <tekkub@gmail.com>
2008-07-06 18:39:12 -0600

    Flesh out container behavior, button update callbacks

--- Quickie.lua
+++ Quickie.lua
@@ -6,7 +6,7 @@
 local L = setmetatable({}, {__index=function(t,i) return i end})
 local defaults, defaultsPC, db, dbpc = {}, {}
 local ldb = LibStub:GetLibrary("LibDataBroker-1.1")
-
+local BUTTONSIZE, EDGE, GAP = 32, 5, 2
 
 ------------------------------
 --      Util Functions      --
@@ -77,28 +77,46 @@ end
 -------------------------------
 
 local container = CreateFrame("Button", nil, UIParent)
-container:SetWidth(40) container:SetHeight(32+5+5)
-container:SetPoint("RIGHT", UIParent, "LEFT", 5, 0)
+container:SetWidth(EDGE*2) container:SetHeight(BUTTONSIZE + EDGE*2)
+container:SetPoint("RIGHT", UIParent, "LEFT", EDGE, 0)
 
 container:SetBackdrop({
 	bgFile = "Interface\\Tooltips\\UI-Tooltip-Background",
 	edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
 	edgeSize = 16,
-	insets = {left = 5, right = 5, top = 5, bottom = 5},
+	insets = {left = EDGE, right = EDGE, top = EDGE, bottom = EDGE},
 	tile = true, tileSize = 16,
 })
 container:SetBackdropColor(0.09, 0.09, 0.19, 0.5)
 container:SetBackdropBorderColor(1, 1, 0.5, 0.5)
 
-container:SetScript("OnEnter", function(self)
-	self:ClearAllPoints()
-	self:SetPoint("LEFT", UIParent, "LEFT", -5, 0)
-end)
 
-container:SetScript("OnLeave", function(self)
+local hideelapsed = 0
+local function OnUpdate(self, elapsed)
+	hideelapsed = hideelapsed + elapsed
+	if hideelapsed < 2 then return end
+
 	self:ClearAllPoints()
-	self:SetPoint("RIGHT", UIParent, "LEFT", 5, 0)
-end)
+	self:SetPoint("RIGHT", UIParent, "LEFT", EDGE, 0)
+	self:SetScript("OnUpdate", nil)
+end
+
+
+local function containerOnEnter()
+	container:SetScript("OnUpdate", nil)
+	container:ClearAllPoints()
+	container:SetPoint("LEFT", UIParent, "LEFT", -EDGE, 0)
+end
+
+
+local function containerOnLeave()
+	hideelapsed = 0
+	container:SetScript("OnUpdate", OnUpdate)
+end
+
+
+container:SetScript("OnEnter", containerOnEnter)
+container:SetScript("OnLeave", containerOnLeave)
 
 
 -----------------------------
@@ -114,52 +132,73 @@ local function GetTipAnchor(frame)
 end
 
 
-local function OnEnter(self)
- 	GameTooltip:SetOwner(self, "ANCHOR_NONE")
-	GameTooltip:SetPoint(GetTipAnchor(self))
-	GameTooltip:ClearLines()
+local function OnEnter(self, ...)
+	containerOnEnter()
 
-	local name, title, notes = GetAddOnInfo(self.dataobj.tocname or self.doname)
-	if name then
-		GameTooltip:AddLine(title or name)
-		GameTooltip:AddLine(notes)
+	if self.dataobj.OnEnter then self.dataobj.OnEnter(self, ...)
 	else
-		GameTooltip:AddLine(self.dataobj.label or self.doname)
-	end
+		GameTooltip:SetOwner(self, "ANCHOR_NONE")
+		GameTooltip:SetPoint(GetTipAnchor(self))
+		GameTooltip:ClearLines()
 
-	GameTooltip:Show()
+		local name, title, notes = GetAddOnInfo(self.dataobj.tocname or self.doname)
+		if name then
+			GameTooltip:AddLine(title or name)
+			GameTooltip:AddLine(notes)
+		else
+			GameTooltip:AddLine(self.dataobj.label or self.doname)
+		end
+
+		GameTooltip:Show()
+	end
 end
 
 
-local function OnLeave() GameTooltip:Hide() end
+local function OnLeave(self, ...)
+	containerOnLeave()
+	if self.dataobj.OnLeave then self.dataobj.OnLeave(self, ...)
+	else GameTooltip:Hide() end
+end
 
 
+local function IconChanged(self, event, name, key, value, dataobj)
+	self.texture:SetTexture(value)
+end
+
+
+local function OnClickChanged(self, event, name, key, value, dataobj)
+	self:SetScript("OnClick", value)
+end
+
+
+local frames, lastframe = {}
 function f:NewDataobject(event, name, dataobj)
 	dataobj = dataobj or ldb:GetDataObjectByName(name)
 	if not dataobj.launcher then return end
 
-	local frame = CreateFrame("Button", nil, UIParent)
-	frame:SetWidth(32) frame:SetHeight(32)
-	frame:SetPoint("TOPLEFT", 100, -100)
-	frame.doname, frame.dataobj = name, dataobj
+	local frame = CreateFrame("Button", nil, container)
+	frame:SetWidth(BUTTONSIZE) frame:SetHeight(BUTTONSIZE)
+	frame:SetPoint("LEFT", lastframe or container, lastframe and "RIGHT" or "LEFT", lastframe and GAP or EDGE, 0)
+	container:SetWidth(container:GetWidth() + BUTTONSIZE + (lastframe and GAP or 0))
+	frame.doname, frame.dataobj, frame.IconChanged, frame.OnClickChanged = name, dataobj, IconChanged, OnClickChanged
 
-	frame:SetBackdrop({
-		bgFile = "Interface\\Tooltips\\UI-Tooltip-Background",
-		edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
-		edgeSize = 16,
-		insets = {left = 5, right = 5, top = 5, bottom = 5},
-		tile = true, tileSize = 16,
-	})
-
-	frame:SetScript("OnEnter", dataobj.OnEnter or OnEnter)
-	frame:SetScript("OnLeave", dataobj.OnLeave or OnLeave)
+	frame:SetScript("OnEnter", OnEnter)
+	frame:SetScript("OnLeave", OnLeave)
 	frame:SetScript("OnClick", dataobj.OnClick)
 
 	frame.texture = frame:CreateTexture()
 	frame.texture:SetAllPoints()
 	frame.texture:SetTexture(dataobj.icon)
 
---~ 	ldb.RegisterCallback(frame, "LibDataBroker_AttributeChanged_"..dataobjname.."_text", "TextUpdate")
+	ldb.RegisterCallback(frame, "LibDataBroker_AttributeChanged_"..name.."_icon", "IconChanged")
+	ldb.RegisterCallback(frame, "LibDataBroker_AttributeChanged_"..name.."_OnClick", "OnClickChanged")
+
+	frames[name], lastframe = frame, frame
 end
 
+
+for name,dataobj in ldb:DataObjectIterator() do if dataobj.launcher then f:NewDataobject(nil, name, dataobj) end end
 ldb.RegisterCallback(f, "LibDataBroker_DataObjectCreated", "NewDataobject")
+ldb.RegisterCallback(f, "LibDataBroker_AttributeChanged__launcher", function(event, name, key, value, dataobj)
+	if value and not frames[name] then f:NewDataobject(nil, name, dataobj) end
+end)
